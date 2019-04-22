c39bb8a425c831f460800ada35a9f96a6c93d3c9
Ismail Koksal <ismail@koksal.fr>
2019-04-22 14:29:10 +0200

    Ajout d'un toolbar et d'un drawer

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <v-navigation-drawer v-model="drawer" fixed app>
+            <Drawer/>
+        </v-navigation-drawer>
+        <v-toolbar fixed app>
+            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        </v-toolbar>
+    </div>
+</template>
+
+<script>
+    import Drawer from '@/components/Drawer'
+
+    export default {
+        name: "Toolbar",
+        data() {
+            return {
+                drawer: false
+            }
+        },
+        components: {Drawer}
+    }
+</script>
+
+<style scoped>
+
+</style>
